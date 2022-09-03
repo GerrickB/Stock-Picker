11238e2ef4e95fefa0850a953e9d97b7123c3d07
Gerrick B <lsgenether@gmail.com>
2022-09-03 03:35:45 -0700

    Edit stock_picker method

--- stock.rb
+++ stock.rb
@@ -1,10 +1,18 @@
 def stock_picker(prices)
-    prices.each_with_index do |price, day|
-        profit = price - prices[0]
-        puts "Day #{day}: #{price} - Day 0: #{prices[0]} = #{profit}"
-    end
+    total_days = prices.length
+    daily_profit = Hash.new(0)
+    #8.times do
+        prices.each_with_index do |price, day|
+            profit = price - prices[0]
+            puts "Day #{day}: #{price} - Day 0: #{prices[0]} = #{profit}"
+            daily_profit[[0,day]] = profit
+        end
+    #end
+    daily_profit
 end
 
 
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+puts stock_picker([17,3,6,9,15,8,6,1,10])
+#use Hash.new
+#number of days.times
