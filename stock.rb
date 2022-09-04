14c3607a58db546a1604446efee9fe8a2c0a9d36
Gerrick B <lsgenether@gmail.com>
2022-09-03 20:31:44 -0700

    Implement working stock_picker method

--- stock.rb
+++ stock.rb
@@ -1,9 +1,38 @@
 def stock_picker(prices)
+    daily_profit = Hash.new(0)
+    buy_day = 0
+    day_adjust = 0
+
+    until prices.eql?([]) do
+        prices.each_with_index do |price, day|
+            profit = price - prices[0]
+            #puts "Day #{day + day_adjust}: #{price} - Day #{buy_day}: #{prices[0]} = #{profit}"
+            daily_profit[[buy_day, day + day_adjust]] = profit
+        end
+        buy_day += 1
+        day_adjust += 1
+        #use shift and prices[0] to skip bought days
+        prices.shift
+    end
+    print daily_profit.key(daily_profit.values.max)
+end
+
+
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+#hasha = stock_picker([17,3,6,9,15,8,6,1,10])
+#print hasha.key(hasha.values.max)
+
+#good alternative
+=begin
+
+def stock_picker(prices)
     total_days = prices.length
     daily_profit = Hash.new(0)
     buy_day = 0
     total_days.times do
         prices.each_with_index do |price, day|
+            if day 
             profit = price - prices[buy_day]
             puts "Day #{day}: #{price} - Day #{buy_day}: #{prices[buy_day]} = #{profit}"
             daily_profit[[buy_day,day]] = profit
@@ -12,11 +41,4 @@ def stock_picker(prices)
     end
     daily_profit
 end
-
-
-
-puts stock_picker([17,3,6,9,15,8,6,1,10])
-#hasha = stock_picker([17,3,6,9,15,8,6,1,10])
-#print hasha.max[0]
-#use Hash.new
-#number of days.times
+=end
