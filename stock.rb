0ff73ac72100632fd861bf19a5edeb935b614c69
gerrickb <lsgenether@gmail.com>
2022-09-25 17:13:51 -0700

    Delete block comments

--- stock.rb
+++ stock.rb
@@ -22,23 +22,3 @@ end
 stock_picker([17,3,6,9,15,8,6,1,10])
 #hasha = stock_picker([17,3,6,9,15,8,6,1,10])
 #print hasha.key(hasha.values.max)
-
-#good alternative
-=begin
-
-def stock_picker(prices)
-    total_days = prices.length
-    daily_profit = Hash.new(0)
-    buy_day = 0
-    total_days.times do
-        prices.each_with_index do |price, day|
-            if day 
-            profit = price - prices[buy_day]
-            puts "Day #{day}: #{price} - Day #{buy_day}: #{prices[buy_day]} = #{profit}"
-            daily_profit[[buy_day,day]] = profit
-        end
-        buy_day += 1
-    end
-    daily_profit
-end
-=end
